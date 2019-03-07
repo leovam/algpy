5d07d96367302c9d38a8ad26efb5f312ad98550b
Leo <leovam171@gmail.com>
2019-03-06 23:05:45 -0500

    doubly linked list base

---
+++ pesudo&code/DoubleLinkedList.py
@@ -0,0 +1,41 @@
+class _DoublyLinkedBase:
+    class _Node:
+        __slots__ = '_element', '_prev', '_next'
+
+        def __init__(self, element, prev, next):
+            self._element = element
+            self._prev = prev
+            self._next = next
+            
+        
+    def __init__(self):
+        self._header = self._Node(None, None, None)
+        self._trailer = self._Node(None, None, None)
+        self._header._next = self._trailer
+        self._trailer._prev = self._header
+        self._size = 0
+
+    def __len__(self):
+        return self._size
+
+    def is_empty(self):
+        return self._size == 0
+
+    def _insert_bewteen(self, e, pre, suc):
+        newest = self._Node(e, pre, suc)
+        pre._next = newest
+        suc._prev = newest
+        self._size += 1
+        return newest
+
+    def _delete_node(self, node):
+        pre = node._prev
+        suc = node._next
+        pre._next = suc
+        suc._pre = pre
+        self._size -= 1
+        element = node._element
+        node._prev = node._next = node._element = None
+        return element
+
+
