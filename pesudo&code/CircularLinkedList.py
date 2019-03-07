0e5cdcca6aa75ba40b875aee9c230ce1062d3cea
Leo <leovam171@gmail.com>
2019-03-06 21:44:53 -0500

    inplementation of Circular Likned List

---
+++ pesudo&code/CircularLinkedList.py
@@ -0,0 +1,62 @@
+class CircularQueue:
+    class _Node:
+        __slots__ = '_element', '_next'
+        
+        def __init__(self, element, next):
+            self._element = element
+            self._next = next
+
+    def __init__(self):
+        self._tail = None
+        self._size = 0
+
+    def __len__(self):
+        return self._size
+
+    def is_empty(self):
+        return self._size == 0
+
+    def first(self):
+        if self.is_empty():
+            print ("Queue is Empty")
+            
+        head = self._tail._next
+        return head._element
+
+    def dequeue(self):
+        if self.is_empty():
+            print ("Queue is Empty")
+        
+        oldhead = self._tail._next
+        if self._size == 1:
+            self._tail = None
+        else:
+            self._tail._next = oldhead._next
+
+        self._size -= 1
+        return oldhead._element
+
+    def enqueue(self, e):
+        newest = self._Node(e,None)
+        if self.is_empty():
+            newest._next = newest
+        else:
+            newest._next = self._tail._next
+            self._tail._next = newest
+        self._tail = newest
+        self._size += 1
+
+    def rotate(self):
+        if self._size > 0:
+            self._tail = self._tail._next
+
+
+if __name__ == '__main__':
+    test = CircularQueue()
+    print (test.is_empty())
+
+    test.enqueue(3)
+    test.enqueue(4)
+
+    print (test.rotate())
+    print (test.first())
