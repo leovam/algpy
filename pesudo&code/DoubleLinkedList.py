2e1274a2246e3ae27964ab87b4e27d0adb1bbafd
Leo <leovam171@gmail.com>
2019-03-08 22:46:26 -0500

    inplement double-ended queue

--- pesudo&code/DoubleLinkedList.py
+++ pesudo&code/DoubleLinkedList.py
@@ -38,4 +38,38 @@ class _DoublyLinkedBase:
         node._prev = node._next = node._element = None
         return element
 
+class LinkedDeque(_DoublyLinkedBase):
+    def first(self):
+        if self.is_empty():
+            print ("Dequeue is Empty")
+        return self._header._next._element
+
+    def last(self):
+        if self.is_empty():
+            print ("Dequeue is Empty")
+        return self._trailer._prev._element
+
+    def insert_first(self, e):
+        self._insert_bewteen(e, self._header, self._header._next)
+    
+    def insert_last(self, e):
+        self._insert_bewteen(e, self._trailer._prev, self._trailer)
+
+    def delete_first(self):
+        if self.is_empty():
+            print ("Dequeue is Empty")
+        return self._delete_node(self._header._next)
+
+    def delete_last(self):
+        if self.is_empty():
+            print ("Dequeue is Empty")
+        return self._delete_node(self._trailer._prev)
+    
+if __name__ == '__main__':
+    test = LinkedDeque()
+    test.insert_first(1)
+    test.insert_first(2)
+    test.insert_first(3)
+    test.insert_first(4)
+    print (test.first())
 
