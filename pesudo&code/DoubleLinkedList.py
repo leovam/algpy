8aa56fa461dee440849e32b41f9c002054e604ba
Leo <leovam171@gmail.com>
2019-03-09 21:02:53 -0500

    add some rule for positional list, not finished yet

--- pesudo&code/DoubleLinkedList.py
+++ pesudo&code/DoubleLinkedList.py
@@ -65,11 +65,37 @@ class LinkedDeque(_DoublyLinkedBase):
             print ("Dequeue is Empty")
         return self._delete_node(self._trailer._prev)
     
-if __name__ == '__main__':
-    test = LinkedDeque()
-    test.insert_first(1)
-    test.insert_first(2)
-    test.insert_first(3)
-    test.insert_first(4)
-    print (test.first())
 
+class PositionalList(_DoublyLinkedBase):
+    class Position:
+        def __init__(self,  container, node):
+            self._container = container
+            self._node = node
+
+        def element(self):
+            return self._node._element
+
+        def __eq__(self, other):
+            return type(other) is type(self) and other._node is self._node
+
+        def __ne__(self, other):
+            return not (self==other)
+
+    def _validate(self, p):
+        if not isinstance(p,self.Position):
+            raise TypeError("p must be proper Position type")
+        if p._container is not self:
+            raise ValueError("p does not belong to this container")
+        if p._node._next is None:
+            raise ValueError("p is no longer valid")
+        return p._node
+    
+    def _make_position(self, node):
+        if node is self._header or node is self._trailer:
+            return None
+        else:
+            return self.Position(self, node)
+
+    
+if __name__ == '__main__':
+    pass
