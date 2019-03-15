041d07455266fcbd422532ee4ad82da5f9cd03dc
Leo <leovam171@gmail.com>
2019-03-15 18:21:12 -0400

    add ADT for BT

--- pesudo&code/tree.py
+++ pesudo&code/tree.py
@@ -55,3 +55,28 @@ class Tree:
         if p is None:
             p = self.root()
         return self._height2(p)
+
+    class BinaryTree(Tree):
+        def left(self,p):
+            return NotImplementedError("Must be implemented by subclass")
+
+        def right(self,p):
+            return NotImplementedError("Must be implemented by subclass")
+
+        def sibling(self, p):
+            parent = self.parent(p)
+            if parent is None:
+                return None
+            else:
+                if p == self.left(parent):
+                    return self.right(parent)
+                else:
+                    return self.left(parent)
+
+        def children(self,p):
+            if self.left(p) is not None:
+                yield self.left(p)
+            if self.right(p) is not None:
+                yield self.right(p)
+
+        
